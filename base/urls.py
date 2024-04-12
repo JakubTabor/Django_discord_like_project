668622c4092aacd9cadc6e21b916c7fe104c0849
Jakub <124591894+JakubTabor@users.noreply.github.com>
2024-04-12 13:09:49 +0100

    Add files via upload

--- base/urls.py
+++ base/urls.py
@@ -1,16 +1,13 @@
-from django.urls import path
-from . import views
+"""
+
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+
+"""
+from django.contrib import admin
+from django.urls import path, include
+    
 
 urlpatterns = [
-  path('login/', views.loginPage, name="login"),
-  path('logout/', views.logoutUser, name="logout"),
-  path('register/', views.registerPage, name="register"),
-  
-  path('', views.home, name="home"), 
-  path('room/<str:pk>/', views.room, name="room"),
-  
-  path('create-room/', views.createRoom, name="create-room"),
-  path('update-room/<str:pk>/', views.updateRoom, name="update-room"),
-  path('delete-room/<str:pk>/', views.deleteRoom, name="delete-room"),
-  path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
+    path('admin/', admin.site.urls),
+    path('', include('base.urls'))
 ]
